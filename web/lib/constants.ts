48b8efd9417fcd504379f0db9d9b25d680b1039c
hyukkyukang <hyukkyukang@gmail.com>
2023-12-12 10:46:38 +0900

    update: demo video url

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,4 +1,4 @@
 export const DEPLOY_URL = "https://sqlbot.co.kr";
 export const DEMO_URL = "https://sqlbot.co.kr/conversation";
-export const DEMO_VIDEO_URL = "https://www.youtube.com/watch?v=kR6kAHNXm7Q";
+export const DEMO_VIDEO_URL = "https://youtu.be/zPdGebsV1s4";
 export const LAB_URL = "https://dslab.postech.ac.kr";
