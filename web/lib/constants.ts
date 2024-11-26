9ce55fb514b87dbd72595ab4f106c6032df49858
pshlego <parksh0712@postech.ac.kr>
2024-11-26 17:03:52 +0900

    change address

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,4 +1,6 @@
-export const DEPLOY_URL = "https://sqlbot.co.kr";
-export const DEMO_URL = "https://sqlbot.co.kr/conversation";
+// export const DEPLOY_URL = "https://sqlbot.co.kr";
+export const DEPLOY_URL = "http://localhost:3000";
+// export const DEMO_URL = "https://sqlbot.co.kr/conversation";
+export const DEMO_URL = "http://localhost:3000/conversation";
 export const DEMO_VIDEO_URL = "https://youtu.be/zPdGebsV1s4";
 export const LAB_URL = "https://dslab.postech.ac.kr";
